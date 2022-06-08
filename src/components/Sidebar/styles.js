5d5bcfbbde43f9a548430d46744c2550004201c0
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-06-08 13:46:43 -0300

    continuando sidebar

--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Search } from '../../styles/Icons';
 
 export const Container = styled.div`
@@ -24,8 +24,36 @@ export const SearchWrapper = styled.div`
   max-height: 57px;
 `;
 
-export const SearchInput = styled.div`
+export const SearchInput = styled.input`
   width: 100%;
+  height: 39px;
+  font-size: 14px;
+  padding: 0 10px 0 52px;
+  border-radius: 19.5px;
+  background: var(--search);
+
+  &::placeholder {
+    color: var(--gray);
+  }
+
+  ~ svg {
+    position: relative;
+    top: -33px;
+    left: 15px;
+    z-index: 1;
+
+    transition: 180ms ease-in-out;
+  }
+
+  outline: 0;
+
+  &:focus {
+    border: 1px solid var(--twitter);
+
+    ~ svg {
+      fill: var(--twitter);
+    }
+  }
 `;
 
 export const SearchIcon = styled(Search)`
@@ -35,4 +63,9 @@ export const SearchIcon = styled(Search)`
   fill: var(--gray);
 `;
 
-export const Body = styled.div``;
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 57px 24px 200px;
+  margin-top: 3px;
+`;
